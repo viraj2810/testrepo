a4b20ded70405da0d047dc6ca14a060837cec578
viraj2810 <98727805+viraj2810@users.noreply.github.com>
2022-05-31 22:45:41 +0530

    Add files via upload

---
+++ complete_class.java
@@ -0,0 +1,20 @@
+package Abstract_class;
+
+public class complete_class
+{
+	//complete methods
+   public void m1()             //method declartion
+   {
+	   System.out.println("Hi"); //method defination
+   }
+	
+  //complete methods
+   public void m2()             //method declartion
+   {
+	   System.out.println("Hello"); //method defination
+   }
+	
+   
+   
+   
+}
